5ed61525da7a7d1f4bb614eff52134edf5fc9334
zubin | zeuscoder <zubin.pratap@gmail.com>
2019-01-08 18:02:26 +1100

    fix error in server port config for heroku. create new git repo just for server/heroku CLI deploy

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send({ body: "Hi, there!" });
