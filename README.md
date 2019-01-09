8c099dacba74caef0b83944fe889e059aa74034c
zubin | zeuscoder <zubin.pratap@gmail.com>
2019-01-09 11:25:52 +1100

    setup passport google auth strategy. configure. add google keys to secret.js. gitignore secret.js. WORKING

---
+++ README.md
@@ -0,0 +1,4 @@
+# GIT REPOS
+The 'master' git repo is initialised in the root of the project. However, the server is deployed to heroku and the heroku CD git repo is initalised in /server.  Thus, there are two git repos co-existing in this project.
+
+The secret.js file is gitignored in the __root level__ git repo.
--- server/index.js
+++ server/index.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const CONFIG = require("./secret.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: CONFIG.googleClientID,
+      clientSecret: CONFIG.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
 app.get("/", (req, res) => {
+  console.log("\nroot endpoint hit...\n");
   res.send({ body: "Hi, there!" });
 });
 
