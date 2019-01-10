5878632be6a8f10542f357072500912873db41ae
zubin | zeuscoder <zubin.pratap@gmail.com>
2019-01-10 14:42:56 +1100

    implement google auth passport callback. check if user already exists in db. save only if not. WORKING

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose')
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
+const userSchemaConfig = { strict: false };
 
-const userSchema  = new Schema({
+const userSchema = new Schema(
+  {
     googleID: String
-});
+  },
+  userSchemaConfig
+);
 
-mongoose.model('users', userSchema);
+mongoose.model("users", userSchema);
--- server/services/passportConfig.js
+++ server/services/passportConfig.js
@@ -17,15 +17,29 @@ passport.use(
   )
 );
 
-function successCallback(accessToken, refreshToken, profile, done)  {
+function successCallback(accessToken, refreshToken, profile, done) {
+  //check db for user and save if not previously registered
+  User.findOne({ googleID: profile.id })
+    .then(existingUser => {
+      //if not null
+      if (!existingUser) {
+        console.log("New user. saving to DB.... at...\n", __dirname);
+        new User({
+          googleID: profile.id,
+          name: profile.displayName,
+          email: profile.emails[0].value
+        }).save();
+      } else {
+        console.log("User already exists: ", profile.id, " at...\n", __dirname);
+      }
+    })
+    .catch(err =>
+      console.log("error checking if user exists" + " ...at.... " + __dirname)
+    );
+
   // console.log("accessToken : ", accessToken);
   // console.log("refreshToken : ", refreshToken);
   // console.log("Google profile ID: ", profile.id);
   // // console.log("done : ", done);
   // console.log("\nLOG statements are AT : ", __dirname);
-  new User({
-    googleID: profile.id ,
-    // name: profile.displayName,
-    // email: profile.emails[0].value 
-  }).save();
 }
