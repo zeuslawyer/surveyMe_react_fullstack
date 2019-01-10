30b3478d571f35b4e13a759bcffef7be1dae0c19
zubin | zeuscoder <zubin.pratap@gmail.com>
2019-01-10 15:03:03 +1100

    implement done() passport callback to handle saving user to DB logic. WORKING

--- server/services/passportConfig.js
+++ server/services/passportConfig.js
@@ -20,22 +20,33 @@ passport.use(
 function successCallback(accessToken, refreshToken, profile, done) {
   //check db for user and save if not previously registered
   User.findOne({ googleID: profile.id })
-    .then(existingUser => {
-      //if not null
-      if (!existingUser) {
-        console.log("New user. saving to DB.... at...\n", __dirname);
-        new User({
-          googleID: profile.id,
-          name: profile.displayName,
-          email: profile.emails[0].value
-        }).save();
-      } else {
-        console.log("User already exists: ", profile.id, " at...\n", __dirname);
-      }
-    })
-    .catch(err =>
-      console.log("error checking if user exists" + " ...at.... " + __dirname)
+  .then(existingUser => {
+    //if null , i.e. no such user in db...
+    if (!existingUser) {
+      new User({
+        googleID: profile.id,
+        name: profile.displayName,
+        email: profile.emails[0].value
+      })
+        .save()
+        .then(user => {
+          console.log("New user. Saved to DB.... at... ", __dirname);
+          done(null, user);
+        });
+    } else {
+      //if not null i.e. user already exists in db...
+      console.log("User already exists: ", profile.id, " at... ", __dirname);
+      done(null, existingUser);
+    }
+  })
+  .catch(err => {
+    console.log(
+      "promise-related error when checking if user exists" +
+        " ...at.... " +
+        __dirname
     );
+    done(err, null);
+  })
 
   // console.log("accessToken : ", accessToken);
   // console.log("refreshToken : ", refreshToken);
