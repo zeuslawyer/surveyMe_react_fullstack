47bfdf188434fe49f5ee40bd31906145384e2912
zubin | zeuscoder <zubin.pratap@gmail.com>
2019-01-10 12:55:30 +1100

    implement mongoose, User schema, and google auth successcallback write to db. WORKING

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const CONFIG = require('./secret.js')
+const mongoose = require('mongoose')
+
+//WIRE UP MONGOOSE
+require('./models/User.js')
+mongoose.connect(CONFIG.mongoURI);
 
 //load the passport config
 require("./services/passportConfig.js");
 
+
+//SET UP SERVER
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//load the exported routing middleware AND immediately invoke
+//load the exported routing middleware functions AND immediately invoke
 require('./routes/index.js')(app)
 require('./routes/authRoutes.js')(app)
 
--- server/services/passportConfig.js
+++ server/services/passportConfig.js
@@ -1,6 +1,10 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const CONFIG = require("../secret.js");
+const mongoose = require("mongoose");
+
+//retrieve the User Class/ Model from mongoose
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -9,12 +13,19 @@ passport.use(
       clientSecret: CONFIG.googleClientSecret,
       callbackURL: "/auth/google/callback" //this must match with the google console setup for oauth URL redirect
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken : ", accessToken);
-      console.log("refreshToken : ", refreshToken);
-      console.log("profile : ", profile);
-      console.log("done : ", done);
-      console.log("\nLOG statements are AT : ", __dirname);
-    }
+    successCallback
   )
 );
+
+function successCallback(accessToken, refreshToken, profile, done)  {
+  // console.log("accessToken : ", accessToken);
+  // console.log("refreshToken : ", refreshToken);
+  // console.log("Google profile ID: ", profile.id);
+  // // console.log("done : ", done);
+  // console.log("\nLOG statements are AT : ", __dirname);
+  new User({
+    googleID: profile.id ,
+    // name: profile.displayName,
+    // email: profile.emails[0].value 
+  }).save();
+}
