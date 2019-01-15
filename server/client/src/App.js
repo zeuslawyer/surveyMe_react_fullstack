295998de63fb12fc064488be12bef6e52b8c449f
zubin | zeuscoder <zubin.pratap@gmail.com>
2019-01-15 13:19:48 +1100

    implement server proxy for client server in setupProxy.js. refactor passport config to use async/await. WORKING

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <a href="/auth/google">SIGN IN WITH GOOGLE</a>
           <a
             className="App-link"
             href="https://reactjs.org"
--- server/services/passportConfig.js
+++ server/services/passportConfig.js
@@ -8,7 +8,7 @@ const User = mongoose.model("users");
 
 //serialise user for creating ID token for user in-browser
 passport.serializeUser((user, done) => {
-  //user here is the mongo record , not the Google profile.
+  //user here is the mongo record , not the Google profile. user ID is encoded
   done(null, user.id);
 });
 
@@ -16,59 +16,50 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((id, done) => {
   User.findById(id).then(userRecord => done(null, userRecord));
 });
+
 //set up Google Strategy
 passport.use(
   new GoogleStrategy(
     {
       clientID: CONFIG.googleClientID,
       clientSecret: CONFIG.googleClientSecret,
-      callbackURL: "/auth/google/callback", 
-      proxy: true 
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     successCallback
   )
 );
 
 //callback that gets invoked when Passport successfully retrieves users credentials from Google
-function successCallback(accessToken, refreshToken, profile, done) {
-  //check db for user and save if not previously registered
-  User.findOne({ googleID: profile.id })
-    .then(existingUser => {
-      //if null , i.e. no such user in db...
-      if (!existingUser) {
-        new User({
-          googleID: profile.id,
-          name: profile.displayName,
-          email: profile.emails[0].value
-        })
-          .save()
-          .then(user => {
-            console.log("New user. Saved to DB.... at... ", __filename);
-            done(null, user);
-          });
-      } else {
-        //if not null i.e. user already exists in db...
-        console.log(
-          "User already exists. GoogleID: ",
-          profile.id,
-          " at... ",
-          __filename
-        );
-        done(null, existingUser);
-      }
-    })
-    .catch(err => {
-      console.log(
-        "promise-related error when checking if user exists" +
-          " ...at.... " +
-          filename
-      );
-      done(err, null);
-    });
+async function successCallback(accessToken, refreshToken, profile, done) {
+  //if the user does not exist in DB...
+  const existingUser = await User.findOne({ googleID: profile.id });
+  if (!existingUser) {
+    const user = await new User({
+      googleID: profile.id,
+      name: profile.displayName,
+      email: profile.emails[0].value
+    }).save();
+    console.log("New user. Saved to DB....at... ", __filename);
+    return done(null, user);
+  }
 
-  // console.log("accessToken : ", accessToken);
-  // console.log("refreshToken : ", refreshToken);
-  // console.log("Google profile ID: ", profile.id);
-  // // console.log("done : ", done);
-  // console.log("\nLOG statements are AT : ", __filename);
+  //if  user exists in DB
+  console.log("User already exists - not saving to DB. GoogleID: ", profile.id);
+  console.log(" at filename... ", __filename);
+  done(null, existingUser);
 }
+// .catch(err => {
+//   console.log(
+//     "promise-related error when checking if user exists" +
+//       " ...at.... " +
+//       filename
+//   );
+//   done(err, null);
+// });
+
+// console.log("accessToken : ", accessToken);
+// console.log("refreshToken : ", refreshToken);
+// console.log("Google profile ID: ", profile.id);
+// // console.log("done : ", done);
+// console.log("\nLOG statements are AT : ", __filename);
