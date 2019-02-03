a3493096db8e11d34c29c021b8e6d3b27bcc3892
zubin | zeuscoder <zubin.pratap@gmail.com>
2019-02-04 10:07:27 +1100

    re done Mailer.js file. WORKING! using old api for sendgrid

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -1,70 +1,54 @@
-const helper = require("@sendgrid/helpers").classes;
-const sendgrid = require("sendgrid");
-const helper2 = sendgrid.mail;
-const { sendGridKey } = require("../config/secrets");
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/secrets');
 
-// designed to work for things other than just surveys
+class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();
 
-const SELECTED_HELPER = helper2;
+        this.sgApi = sendgrid(keys.sendGridKey);
+        this.from_email = new helper.Email('no-reply@emaildaniel.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
 
-class Mailer extends SELECTED_HELPER.Mail {
-  /*
-    to instantiate an object of this class Mailer, the constructor must receive
-    two arguments:  1) an object with props of 'subject' and 'recipients', and
-    2) a string of HTML that will be rendered as the email body
-*/
-  constructor({ subject, recipients }, content) {
-    super();
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
+    }
 
-    this.sgApi = sendgrid(sendGridKey);
-    this.subject = subject;
-    this.recipients = this.formatAddresses(recipients); //recipients are the array from Survey data model
-    this.body = new SELECTED_HELPER.Content("text/html", content);
-    this.from_email = new SELECTED_HELPER.Email("no-reply@surveyme.com");
+    formatAddresses(recipients) {
+        return recipients.map(({ email })=> {
+            return new helper.Email(email);
+        });
+    }
 
-    //add sendgrid click tracking on links inside email...as per sendgrid docs
-    this.addContent(this.body);
-    this.addClickTracking();
-    this.addRecipients();
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
 
-    //end constructor
-  }
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
 
-  //ES6 fat arrow not allowed natively by ES Lint without configuration of babelrc...
-  formatAddresses(recipients) {
-    return recipients.map(recip => {
-      return new SELECTED_HELPER.Email(recip.email);
-    });
-  }
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
 
-  addClickTracking() {
-    const trackingSettings = new SELECTED_HELPER.TrackingSettings();
-    const clickTracking = new SELECTED_HELPER.ClickTracking(true, true);
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
 
-    trackingSettings.setClickTracking(clickTracking);
-    this.addTrackingSettings(trackingSettings);
-  }
-
-  addRecipients() {
-    const personalize = new helper.Personalization();
-    this.recipients.forEach(recipient => {
-      personalize.addTo(recipient);
-    });
-    this.addPersonalization(personalize);
-  }
-
-  async send() {
-    //configure
-    const request = this.sgApi.emptyRequest({
-      method: "POST",
-      path: "v3/mail/send",
-      body: this.toJSON()
-    });
-    //send to sendgrid
-    const response = this.sgApi.API(request);
-    //return response as promise
-    return response;
-  }
+        const response = this.sgApi.API(request);
+        return response;
+    }
 }
 
 module.exports = Mailer;
