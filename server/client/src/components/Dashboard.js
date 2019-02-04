31d68a718aeee06640900164c65e040e66c538d7
zubin | zeuscoder <zubin.pratap@gmail.com>
2019-02-04 16:56:42 +1100

    add styling to add new survey FAB

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -4,11 +4,20 @@ import AddIcon from "@material-ui/icons/Add";
 import { Link } from "react-router-dom";
 
 function Dashboard() {
+  const fabStyle = {
+    margin: 0,
+    top: "auto",
+    right: 180,
+    bottom: 70,
+    left: "auto",
+    position: "fixed"
+  };
   return (
-    <div>
+    <div >
       This is the users survey dashboard accessed at route /surveys
-      <div>
+      <div className="container">
         <Fab
+          style={fabStyle}
           color="secondary"
           aria-label="Add"
           component={Link}
